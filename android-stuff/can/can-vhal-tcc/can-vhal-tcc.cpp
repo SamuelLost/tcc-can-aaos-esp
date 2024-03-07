d9543c140dda9e2b5fe6c3614bbd79a3c23efbb1
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2024-03-06 21:31:23 -0300

    Receiving and printing CAN messages
    1) Create a class for SocketCan
    2) Create a logging macros
    3) Remove CAN HAL tests
    
    Signed-off-by: Samuel Henrique <samuelhenriqec@alu.ufc.br>

--- android-stuff/can/can-vhal-tcc/can-vhal-tcc.cpp
+++ android-stuff/can/can-vhal-tcc/can-vhal-tcc.cpp
@@ -1,9 +1,6 @@
 #include <android-base/logging.h>
 #include <log/log.h>
 
-#include <libnetdevice/can.h>
-#include <libnetdevice/libnetdevice.h>
-
 #include <linux/can.h>
 #include <linux/can/error.h>
 #include <linux/can/raw.h>
@@ -14,69 +11,27 @@
 
 #include <memory>
 #include <iostream>
-
-#include <android/hardware/automotive/can/1.0/ICanBus.h> //CanMessage
-#include <android/hardware/automotive/can/1.0/ICanController.h>
-#include <android/hardware/automotive/can/1.0/ICanMessageListener.h>
-#include <android/hardware/automotive/can/1.0/ICloseHandle.h>
-#include <android/hardware/automotive/can/1.0/types.h>
+#include <iomanip>
 
 #include <aidl/android/hardware/automotive/vehicle/BnVehicle.h>
 #include <AidlVhalClient.h>
+
+#include "SocketCan.h"
+#define DEBUG_SOCKET_CAN
+#include "Logging.h"
 
 constexpr static char AIDL_VHAL_SERVICE[] = "android.hardware.automotive.vehicle.IVehicle/default";
 constexpr static char TAG[] = "VHAL_SOCKET";
 
 using ::android::frameworks::automotive::vhal::IVhalClient;
 using ::android::sp;
-using ::android::hardware::automotive::can::V1_0::ICanBus;
-using ::android::hardware::automotive::can::V1_0::ICanController;
-using ::android::hardware::automotive::can::V1_0::ICanMessageListener;
-using ::android::hardware::automotive::can::V1_0::ICloseHandle;
-using ::android::hardware::automotive::can::V1_0::CanMessage;
-using ::android::hardware::automotive::can::V1_0::CanMessageFilter;
-
-// using namespace android::hardware::automotive::can::V1_0::implementation;
-class ListenCallback : public ICanMessageListener {
-public:
-    // Implemente a função de retorno da interface
-    android::hardware::Return<void> onReceive(const CanMessage& message) override {
-        // Implemente o código aqui para tratar as mensagens recebidas.
-        ALOG(LOG_INFO, TAG, "Message received");
-        LOG(INFO) << "Message received";
-        std::cout << "Message received" << std::endl;
-        std::cout << android::hardware::automotive::can::V1_0::toString(message) << std::endl;
-        return android::hardware::Void();
-    }
-};
 
 int main() {
-    // Aqui eu consegui fazer sozinho com base no da eldorado
-    CanMessage message;
-    sp<ICanBus> can_bus = ICanBus::getService();
-    sp<ICanController> can_controller = ICanController::getService();
-    if (can_bus == nullptr || can_controller == nullptr) {
-        ALOG(LOG_ERROR, TAG, "Failed to get CAN service");
-        std::cout << "Failed to get CAN service" << std::endl;
-        return 1;
-    }
-    //---------------
-
-    // Aqui comecei a penar kkkk, recorri ao chat
-    ICanController::BusConfig::InterfaceId::Socketcan socketcan_interface;
-    socketcan_interface.ifname("can0");
-    ICanController::BusConfig config = {
-        .name = "can0",
-        .interfaceId = ICanController::BusConfig::InterfaceId{},
-        .bitrate = 500000,
-    };
-    config.interfaceId.socketcan(socketcan_interface);
-    //---------------
-
-    // Essa função upInterface eu consegui usar sozinho, só não sabia configurar o parametro config
-    if (can_controller->upInterface(config) != ICanController::Result::OK) {
-        ALOG(LOG_ERROR, TAG, "Failed to bring up CAN interface");
-        std::cout << "Failed to bring up CAN interface" << std::endl;
+    
+    tcc::aaos::can::SocketCan socket_can;
+    if(!socket_can.Init()) {
+        ALOG(LOG_ERROR, TAG, "Failed to initialize CAN interface");
+        std::cout << "Failed to initialize CAN interface" << std::endl;
         return 1;
     }
 
@@ -106,28 +61,26 @@ int main() {
 
     ALOG(LOG_VERBOSE, TAG, "VHAL client is AIDL");
     std::cout << "VHAL client is AIDL" << std::endl;
-    
-    // ::android::hardware::hidl_vec<CanMessageFilter> filters;
-    // sp<ICanMessageListener> listener = ICanMessageListener::tryGetService();
-    // Agora a parte de callback eu não consegui fazer sozinho, recorri ao chat e o chat penooooou, passei umas 2h preso nisso
-    sp<ICanMessageListener> listener = new ListenCallback();
-    auto callback = [](android::hardware::automotive::can::V1_0::Result result, const android::sp<android::hardware::automotive::can::V1_0::ICloseHandle>& closeHandle) {
-        // Implemente sua lógica aqui
-        std::cout << "Callback" << std::endl;
-        result = android::hardware::automotive::can::V1_0::Result::OK; // Só para compilar
-        closeHandle.get(); // Só para compilar
-    };
 
-    can_bus->listen({}, listener, callback);
-    //---------------
+    struct can_frame frame;
+
     while (1) {
-        // bus->listen({}, listener, nullptr);
+        if(!socket_can.ReadCanMessage(frame)) {
+            ALOG(LOG_ERROR, TAG, "Failed to read CAN message");
+            LOG_CAN_ERROR(TAG, "Failed to read CAN message");
+        } else {
+            std::cout << std::setfill('0') << std::setw(3) << std::uppercase << std::hex << frame.can_id;
+            std::cout << "\t[" << static_cast<int>(frame.can_dlc) << "]\t";
+            for (int i = 0; i < frame.can_dlc; i++) {
+                std::cout << std::setfill('0') << std::setw(2) << static_cast<int>(frame.data[i]);
+                (i < frame.can_dlc - 1) ? std::cout << " " : std::cout << "";
+            }
+            std::cout << std::endl;
+        }
+
     }
 
     // ALOG(LOG_INFO, TAG, "Sending CAN message");
     // LOG(INFO) << "Sending CAN message";
     // std::cout << "Sending CAN message" << std::endl;
-
-
-
 }
