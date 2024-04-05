66653cd8abc159836c54cd6f4e798bda3d020699
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2024-04-04 21:57:07 -0300

    feat: getting data from CAN Bus and using VHAL props
    1) Function to convert bytes to float (temperature)
    2) Getting VHAL properties
    3) Setting values for VHAL properties
    
    Signed-off-by: Samuel Henrique <samuelhenriqec@alu.ufc.br>

--- android-stuff/can/can-vhal-tcc/can-vhal-tcc.cpp
+++ android-stuff/can/can-vhal-tcc/can-vhal-tcc.cpp
@@ -12,6 +12,7 @@
 #include <memory>
 #include <iostream>
 #include <iomanip>
+#include <endian.h>
 
 #include <aidl/android/hardware/automotive/vehicle/BnVehicle.h>
 #include <AidlVhalClient.h>
@@ -19,9 +20,34 @@
 #include "SocketCan.h"
 #define DEBUG_SOCKET_CAN
 #include "Logging.h"
+#include <byteswap.h>
 
 constexpr static char AIDL_VHAL_SERVICE[] = "android.hardware.automotive.vehicle.IVehicle/default";
 constexpr static char TAG[] = "VHAL_SOCKET";
+
+constexpr static int TEMPERATURE_CAN_ID = 0x124;
+constexpr static int PROP_INFO_TEMPERATURE = 291508224;
+constexpr static int PROP_FAULT_CODE_TEMPERATURE = 286265345;
+
+constexpr static int ACCELEROMETER_CAN_ID = 0x123;
+constexpr static int PROP_INFO_ACCELEROMETER = 289476610;
+constexpr static int PROP_FAULT_CODE_ACCELEROMETER = 286265347;
+
+// 10.0 ºC = 0x00002041 -> Works
+// -273.15 ºC = 0x339388c30000 -> Works
+float BytesToFloat(uint8_t *bytes) {
+    //Funcionou
+    // float f;
+    // uint32_t value = __builtin_bswap32(*(uint32_t *)bytes); // RPI4B é little endian. Essa função converte para big endian
+    // value = htobe32(value); // Converte host byte order para big endian
+    // std::memcpy(&f, &value, sizeof(float));
+    // return f;
+
+    //Funcionou também
+    float f;
+    std::memcpy(&f, bytes, sizeof(float));
+    return f;
+}
 
 using ::android::frameworks::automotive::vhal::IVhalClient;
 using ::android::sp;
@@ -64,23 +90,74 @@ int main() {
 
     struct can_frame frame;
 
+    auto acc_axes = vhal_client->createHalPropValue(PROP_INFO_ACCELEROMETER);
+    auto acc_fault = vhal_client->createHalPropValue(PROP_FAULT_CODE_ACCELEROMETER);
+    auto temp = vhal_client->createHalPropValue(PROP_INFO_TEMPERATURE);
+    auto temp_fault = vhal_client->createHalPropValue(PROP_FAULT_CODE_TEMPERATURE);
+
+    if (acc_axes == nullptr || acc_fault == nullptr || temp == nullptr || temp_fault == nullptr) {
+        ALOG(LOG_ERROR, TAG, "Failed to create HAL property value");
+        std::cout << "Failed to create HAL property value" << std::endl;
+        return 1;
+    }
+
+    int16_t axis_x = 0;
+    int16_t axis_y = 0;
+    int16_t axis_z = 0;
+    float temperature = 0;
+
     while (1) {
+
         if(!socket_can.ReadCanMessage(frame)) {
             ALOG(LOG_ERROR, TAG, "Failed to read CAN message");
             LOG_CAN_ERROR(TAG, "Failed to read CAN message");
         } else {
-            std::cout << std::setfill('0') << std::setw(3) << std::uppercase << std::hex << frame.can_id;
-            std::cout << "\t[" << static_cast<int>(frame.can_dlc) << "]\t";
-            for (int i = 0; i < frame.can_dlc; i++) {
-                std::cout << std::setfill('0') << std::setw(2) << static_cast<int>(frame.data[i]);
-                (i < frame.can_dlc - 1) ? std::cout << " " : std::cout << "";
+            // std::cout << "0x" << std::setfill('0') << std::setw(3) << std::uppercase << std::hex << frame.can_id;
+            // std::cout << "\t[" << static_cast<int>(frame.can_dlc) << "]\t";
+            // for (int i = 0; i < frame.can_dlc; i++) {
+            //     std::cout << std::setfill('0') << std::setw(2) << static_cast<int>(frame.data[i]);
+            //     (i < frame.can_dlc - 1) ? std::cout << " " : std::cout << "";
+            // }
+            // std::cout << std::endl;
+
+            switch (frame.can_id) {
+                case ACCELEROMETER_CAN_ID: {
+                    axis_x = (frame.data[0] << 8) | frame.data[1];
+                    axis_y = (frame.data[2] << 8) | frame.data[3];
+                    axis_z = (frame.data[4] << 8) | frame.data[5];
+                    ALOG(LOG_INFO, TAG, "Axis X: %d, Axis Y: %d", axis_x, axis_y);
+                    std::cout << std::dec << "Axis X: " << axis_x << ", Axis Y: " << axis_y << ", Axis Z: " << axis_z << std::endl;
+                    acc_axes->setInt32Values({axis_x, axis_y, axis_z});
+                    vhal_client->setValueSync(*acc_axes);
+
+                    if(axis_x == 0 && axis_y == 0 && axis_z == 0) {
+                        acc_fault->setStringValue("ACC-E1");
+                        vhal_client->setValueSync(*acc_fault);
+                    } else {
+                        acc_fault->setStringValue("ACC-0");
+                        vhal_client->setValueSync(*acc_fault);
+                    }
+                    break;
+                }
+                case TEMPERATURE_CAN_ID: {
+                    temperature = BytesToFloat(frame.data);
+                    ALOG(LOG_INFO, TAG, "Temperature: %f", temperature);
+                    std::cout << std::dec << "Temperature: " << temperature << std::endl;
+                    temp->setFloatValues({temperature});
+                    vhal_client->setValueSync(*temp);
+                    if (temperature <= -273) {
+                        temp_fault->setStringValue("TMP-E1");
+                        vhal_client->setValueSync(*temp_fault);
+                    } else {
+                        temp_fault->setStringValue("TMP-0");
+                        vhal_client->setValueSync(*temp_fault);
+                    }
+                    break;
+                }
+                default:
+                    break;
             }
-            std::cout << std::endl;
         }
 
     }
-
-    // ALOG(LOG_INFO, TAG, "Sending CAN message");
-    // LOG(INFO) << "Sending CAN message";
-    // std::cout << "Sending CAN message" << std::endl;
 }
