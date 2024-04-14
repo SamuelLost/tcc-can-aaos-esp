1b64b03854a4236dd80ddba3fbb7ae8f4acbefa0
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2024-04-14 15:20:28 -0300

    refactor: create main function App()
    1) Create App() composable function
    2) Add App() on preview
    
    Signed-off-by: Samuel Henrique <samuelhenriqec@alu.ufc.br>

--- android-stuff/App_TCC/app/src/main/java/tcc/samuelhenrique/app/MainActivity.kt
+++ android-stuff/App_TCC/app/src/main/java/tcc/samuelhenrique/app/MainActivity.kt
@@ -3,6 +3,7 @@ package tcc.samuelhenrique.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,85 +35,94 @@ import tcc.samuelhenrique.app.ui.theme.Background_Card
 import tcc.samuelhenrique.app.ui.theme.Subtitulos
 import tcc.samuelhenrique.app.ui.theme.Texto
 
-var temperature = 90F
+var temperature = 100.1F
 var axis = intArrayOf(-996, 15000, 10046)
 var errors = false
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             App_TCCTheme {
-                // A surface container using the 'background' color from the theme
+                App()
+            }
+        }
+    }
+}
+
+@Composable
+fun App() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background_App)
+            .padding(27.dp)
+    ) {
+        Row {
+            Column(
+                modifier = Modifier.width(350.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+//                            verticalArrangement = Arrangement.Top
+
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.carro),
+                    contentDescription = "",
+                    modifier = Modifier.size(
+                        width = 350.dp,
+                        height = 200.dp)
+                )
+                Spacer(modifier = Modifier.padding(30.dp))
+                CardTemperature(temperature)
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+            Card(
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .width(500.dp)
+                    .height(400.dp)
+                    .align(Alignment.CenterVertically)
+                    .verticalScroll(rememberScrollState()),
+                colors = CardDefaults.cardColors(
+                    containerColor = Background_Card,
+                    contentColor = Texto
+                )
+            ) {
                 Column(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .background(Background_App)
-                        .padding(27.dp)
+                        .fillMaxWidth()
+                        .padding(start = 20.dp),
+                    horizontalAlignment = if (errors) Alignment.CenterHorizontally else Alignment.Start
                 ) {
-                    Row {
-                        Text(
-                            text = "Dashboard",
-                            fontSize = 24.sp,
-                            modifier = Modifier
-                                .height(30.dp),
-                            color = Texto,
-                            fontFamily = FontFamily(
-                                Font(R.font.poppins_bold)
-                            )
+                    Text(
+                        text = "Estado do veículo",
+                        fontSize = 24.sp,
+                        modifier = Modifier
+                            .padding(top = 24.dp)
+                            .height(30.dp)
+                            .align(Alignment.Start),
+                        color = Texto,
+                        fontFamily = FontFamily(
+                            Font(R.font.poppins_bold)
                         )
-                        Spacer(modifier = Modifier.padding(100.dp)
+                    )
+                    Text(
+                        text = "Diagnóstico",
+                        fontSize = 16.sp,
+                        modifier = Modifier
+                            .padding(bottom = 38.dp)
+                            .align(Alignment.Start),
+                        color = Subtitulos,
+                        fontFamily = FontFamily(
+                            Font(R.font.poppins_regular)
                         )
-                        Card(
-                            shape = RoundedCornerShape(8.dp),
-                            modifier = Modifier
-                                .width(505.dp)
-                                .height(400.dp)
-                                .align(Alignment.CenterVertically)
-                                .verticalScroll(rememberScrollState()),
-                            colors = CardDefaults.cardColors(
-                                containerColor = Background_Card,
-                                contentColor = Texto
-                            )
-                        )
-                        {
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(start = 28.dp),
-                                horizontalAlignment = if (errors) Alignment.CenterHorizontally else Alignment.Start
-                            ) {
-                                Text(
-                                    text = "Estado do veículo",
-                                    fontSize = 24.sp,
-                                    modifier = Modifier
-                                        .padding(top = 24.dp)
-                                        .height(30.dp)
-                                        .align(Alignment.Start),
-                                    color = Texto,
-                                    fontFamily = FontFamily(
-                                        Font(R.font.poppins_bold)
-                                    )
-                                )
-                                Text(
-                                    text = "Diagnóstico",
-                                    fontSize = 16.sp,
-                                    modifier = Modifier
-                                        .padding(bottom = 38.dp)
-                                        .align(Alignment.Start),
-                                    color = Subtitulos,
-                                    fontFamily = FontFamily(
-                                        Font(R.font.poppins_regular)
-                                    )
-                                )
-                                if(!errors) {
-                                    CardTemperatureError(temperature)
-                                    Spacer(modifier = Modifier.padding(6.dp))
-                                    CardAccelerometerError(axis)
-                                } else {
-                                    NoErrors()
-                            }
-
-                        }
+                    )
+                    if (!errors) {
+                        CardTemperatureError(temperature)
+                        Spacer(modifier = Modifier.padding(6.dp))
+                        CardAccelerometerError(axis)
+                    } else {
+                        NoErrors()
                     }
 
                 }
@@ -119,9 +131,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 1024, heightDp = 600)
 @Composable
 fun Preview() {
     App_TCCTheme {
+        App()
     }
-}}
+}
